86539921515788ec0c7c1cb29be4fe6018b4906e
pronam2000 <>
2026-02-25 19:05:34 +0000

    Variable Naming

--- variables_and_types/variable_naming/main.py
+++ variables_and_types/variable_naming/main.py
@@ -6,5 +6,5 @@ item_name = "Bread"
 print("Item:", "item_name")
 item_price = 4.52
 print("Price per loaf: $", 4.52)
-items_in_stock = 120
-print("Quantity in stock:", 120)
+items_in_stock = 230
+print("Quantity in stock:", 230)
