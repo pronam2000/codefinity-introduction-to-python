970f33906460b0463873e4f859ac53d0bd19e0ad
pronam2000 <>
2026-02-25 18:46:38 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,12 @@
+
+
+
+# Testing
+item_name ="Eggs"
+print("Item : ", item_name)
+item_price =3.99 
+print("Price per unit: $", item_price)
+item_stock_quantity =120
+print("quantity in stock: ", item_stock_quantity)
+in_stock = True
+print("In stock: ", in_stock)
