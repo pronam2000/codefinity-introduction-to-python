c2b656a91c8b296dc56447d622728dd49484ecbb
pronam2000 <>
2026-02-25 19:16:44 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,9 @@
+
+
+
+# Testing
+item_name = "Soda"
+item_price =  6.99
+purchase_quantity = 5
+total_cost = item_price * purchase_quantity
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
